25385a51bfc3ec1234bd38e6b475e454a98f59f6
MisterJYPY <nanoukakoujeanpaul@gmail.com>
2018-03-12 00:44:56 +0100

    quelques migrations inclus

---
+++ app/Http/Controllers/EventManController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EventMan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EventManController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EventMan  $eventMan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EventMan $eventMan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EventMan  $eventMan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EventMan $eventMan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EventMan  $eventMan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EventMan $eventMan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EventMan  $eventMan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EventMan $eventMan)
+    {
+        //
+    }
+}
