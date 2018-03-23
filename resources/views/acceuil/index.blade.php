e054a491148ebe15b9c81a2eb30f563fc562a40b
Rkouadio <rolandkouadio5@gmail.com>
2018-03-23 18:13:10 +0000

    deployement autre pc

--- resources/views/acceuil/index.blade.php
+++ resources/views/acceuil/index.blade.php
@@ -52,7 +52,7 @@
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a id="logo" class="navbar-brand" href="index.html"><img class="" src="images/web_logo.png"
+                            <a id="logo" class="navbar-brand" href="#"><img class="" src="images/logo_web.jpg"
                                                                                      alt=""></a>
                         </div>
                     </div>
@@ -85,7 +85,7 @@
                     <div class="social-shares">
                         <ul>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="{{url('https://www.facebook.com/WeekEndDeLaBoisson/')}}"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="{{url('https://www.facebook.com/WeekEndDeLaBoisson/')}}" target="_blank"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
                             <li><a href="#"><i class="fa fa-skype"></i></a></li>
                         </ul>
